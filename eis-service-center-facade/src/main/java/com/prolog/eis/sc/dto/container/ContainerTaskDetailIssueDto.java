3e7e9db749ed9c7dafc723f18842fd254bce2761
佘周洲 <shezhouzhou@prolog-int.com>
2021-08-18 14:22:51 +0800

    容器任务对外暴露实体

---
+++ eis-service-center-facade/src/main/java/com/prolog/eis/sc/dto/container/ContainerTaskDetailIssueDto.java
@@ -0,0 +1,9 @@
+package com.prolog.eis.sc.dto.container;
+
+/**
+ * @Describe
+ * @Author clarence_she
+ * @Date 2021/8/18
+ **/
+public class ContainerTaskDetailIssueDto {
+}
--- eis-service-center-service/src/main/java/com/prolog/eis/sc/container/ContainerTaskService.java
+++ eis-service-center-service/src/main/java/com/prolog/eis/sc/container/ContainerTaskService.java
@@ -6,4 +6,8 @@ package com.prolog.eis.sc.container;
  * @Date 2021/8/18
  **/
 public interface ContainerTaskService {
+
+
+    boolean containerTaskIssue()throws Exception;
+
 }
