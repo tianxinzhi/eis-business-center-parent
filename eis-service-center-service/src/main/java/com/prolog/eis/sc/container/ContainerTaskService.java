87f7cf4fe2c7648e77c643d1832e526263df32a9
佘周洲 <shezhouzhou@prolog-int.com>
2021-08-18 12:12:07 +0800

    改名字和路径

---
+++ eis-service-center-service/src/main/java/com/prolog/eis/sc/container/ContainerTaskService.java
@@ -0,0 +1,9 @@
+package com.prolog.eis.sc.container;
+
+/**
+ * @Describe 容器任务
+ * @Author clarence_she
+ * @Date 2021/8/18
+ **/
+public interface ContainerTaskService {
+}
---
+++ eis-service-center-service/src/main/java/com/prolog/eis/sc/container/impl/ContainerTaskServiceImpl.java
@@ -0,0 +1,13 @@
+package com.prolog.eis.sc.container.impl;
+
+import com.prolog.eis.sc.container.ContainerTaskService;
+import org.springframework.stereotype.Service;
+
+/**
+ * @Describe
+ * @Author clarence_she
+ * @Date 2021/8/18
+ **/
+@Service
+public class ContainerTaskServiceImpl implements ContainerTaskService {
+}
