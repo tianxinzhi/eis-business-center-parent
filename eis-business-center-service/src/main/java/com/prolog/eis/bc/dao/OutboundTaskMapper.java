dad4371510a49c60277421ef0ca932a4bc8dd680
TianXinzhi <1196780658@qq.com>
2021-09-27 14:43:29 +0800

    解决冲突

--- eis-business-center-service/src/main/java/com/prolog/eis/bc/dao/OutboundTaskMapper.java
+++ eis-business-center-service/src/main/java/com/prolog/eis/bc/dao/OutboundTaskMapper.java
@@ -1,27 +1,11 @@
-<<<<<<< HEAD
-package com.prolog.eis.bc.dao;
-
-import com.prolog.eis.core.model.biz.outbound.OutboundSummaryOrder;
-import com.prolog.eis.core.model.biz.outbound.OutboundTask;
-import com.prolog.framework.dao.mapper.BaseMapper;
-import org.springframework.stereotype.Repository;
-
-/**
- * @Author: txz
- * @Date: 2021/9/24 12:02
- * @Desc:
- */
-@Repository
-public interface OutboundTaskMapper extends BaseMapper<OutboundTask> {
-
-}
-=======
 package com.prolog.eis.bc.dao;
 
 import org.springframework.stereotype.Repository;
 
 import com.prolog.eis.core.model.biz.outbound.OutboundTask;
 import com.prolog.framework.dao.mapper.BaseMapper;
+import org.springframework.stereotype.Repository;
+
 
 /**
  * 出库任务Mapper
@@ -32,4 +16,3 @@ import com.prolog.framework.dao.mapper.BaseMapper;
 public interface OutboundTaskMapper extends BaseMapper<OutboundTask> {
 
 }
->>>>>>> 731d0307c76c19e7903dae292de004910c6bb2e8
--- eis-business-center-service/src/main/java/com/prolog/eis/bc/feign/container/EisContainerLocationFeign.java
+++ eis-business-center-service/src/main/java/com/prolog/eis/bc/feign/container/EisContainerLocationFeign.java
@@ -1,9 +1,9 @@
-<<<<<<< HEAD
 package com.prolog.eis.bc.feign.container;
 
 import com.prolog.eis.core.model.biz.route.ContainerLocation;
 import com.prolog.framework.common.message.RestMessage;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -14,7 +14,7 @@ import java.util.List;
  * @Date: 2021/9/24 10:00
  * @Desc: 远程调用容器位置接口服务
  */
-@FeignClient("service-ai-eis-route-center")
+@FeignClient(value = "${prolog.service.route:service-ai-eis-route-center}")
 public interface EisContainerLocationFeign {
 
     /**
@@ -26,18 +26,6 @@ public interface EisContainerLocationFeign {
     @PostMapping("/api/v1/route/location/findByAreaNo")
     RestMessage<List<ContainerLocation>> findByAreaNo(@RequestParam("areaKey") String areaKey, @RequestParam("value")String value);
 
-}
-=======
-package com.prolog.eis.bc.feign.container;
-
-import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-
-import com.prolog.framework.common.message.RestMessage;
-
-@FeignClient(value = "${prolog.service.route:service-ai-eis-route-center}")
-public interface EisContainerLocationFeign {
 
     /**
      * Feign接口-查询已到达的容器数量
@@ -52,4 +40,3 @@ public interface EisContainerLocationFeign {
     RestMessage<Long> findChuKuLxCount(@RequestParam(value = "areaNo", required = false) String areaNo);
 
 }
->>>>>>> 731d0307c76c19e7903dae292de004910c6bb2e8
