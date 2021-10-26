08133472d9c54c6c8c5a1d3167e88d0a319c83bb
佘周洲 <shezhouzhou@prolog-int.com>
2021-10-26 15:01:44 +0800

    配置文件修改

--- eis-business-center-service/src/main/java/com/prolog/eis/bc/service/outboundtask/impl/OutboundTaskDetailHistoryServiceImpl.java
+++ eis-business-center-service/src/main/java/com/prolog/eis/bc/service/outboundtask/impl/OutboundTaskDetailHistoryServiceImpl.java
@@ -3,6 +3,7 @@ package com.prolog.eis.bc.service.outboundtask.impl;
 import java.util.Date;
 import java.util.List;
 
+import com.prolog.eis.bc.service.outboundtask.OutboundTaskDetailHistoryService;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -10,13 +11,11 @@ import org.springframework.util.CollectionUtils;
 
 import com.google.common.collect.Lists;
 import com.prolog.eis.bc.dao.OutboundTaskDetailHisMapper;
-import com.prolog.eis.bc.service.outboundtask.OutboundTaskDetailHistoryService;
 import com.prolog.eis.core.model.biz.outbound.OutboundTaskDetail;
 import com.prolog.eis.core.model.biz.outbound.OutboundTaskDetailHis;
 
 @Service
-public class OutboundTaskDetailHistoryServiceImpl
-        implements OutboundTaskDetailHistoryService {
+public class OutboundTaskDetailHistoryServiceImpl implements OutboundTaskDetailHistoryService {
 
     @Autowired
     private OutboundTaskDetailHisMapper outboundTaskDetailHisMapper;
